828cc47a649a08f284d2cd814094cf32948cc816
SAVG <1992savg@gmail.com>
2025-04-24 23:03:03 -0300

    chore: Update package.json scripts for type-checking and deployment; add GitHub Actions workflow for AWS deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: { 
-    proxy: {
-      '/api': {
-        target: 'https://ngte2hwp1k.execute-api.us-east-1.amazonaws.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  }
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://ngte2hwp1k.execute-api.us-east-1.amazonaws.com',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				},
+			},
+		},
+	},
+});
 
